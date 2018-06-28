df3015fde361fb755b0e9b7f83f4819badb7a0b9
GONI Guillaume <gdn3@live.fr>
2018-06-28 09:28:35 +0200

    Manager creator to another fun

--- src/mollusque/cephalo/orchestrator/mod.rs
+++ src/mollusque/cephalo/orchestrator/mod.rs
@@ -31,25 +31,7 @@ pub fn orchestrate(listener_rx: mpsc::Receiver<TcpStream>) -> Result<(), Error> 
 
     let (man_tx, man_rx) = mpsc::channel();
 
-    let manager_creator_handle = {
-        let podes_rc_2 = podes_rc.clone();
-        thread::spawn(move || -> Result<(), Error> {
-            for stream in listener_rx.iter() {
-                let mut podes = podes_rc_2.lock().unwrap();
-
-                let id = get_new_id(&mut podes);
-
-                let mut manager = manager::Manager::new(id, stream, man_tx.clone());
-                manager.manage()?;
-
-                podes[id] = Some(manager);
-            }
-
-            println!("Channel from listener closed. Exiting...");
-
-            Ok(())
-        })
-    };
+    /*let manager_creator_handle = */new_manager_creator(podes_rc.clone(), listener_rx, man_tx);
 
     for msg in man_rx.iter() {
         if let manager::Message::Disconnected(id) = msg {
@@ -58,11 +40,12 @@ pub fn orchestrate(listener_rx: mpsc::Receiver<TcpStream>) -> Result<(), Error> 
         }
     }
 
-    match manager_creator_handle.join() {
-        Err(_) => return Err(Error::ThreadPanicked),
-        Ok(Err(err)) => return Err(Error::from(err)),
-        _ => (),
-    };
+    // TODO: Do I need to join the thread (possible problems with the mutex (use of a Weak ?) ?)
+    // match manager_creator_handle.join() {
+    //     Err(_) => return Err(Error::ThreadPanicked),
+    //     Ok(Err(err)) => return Err(Error::from(err)),
+    //     _ => (),
+    // };
 
     Ok(())
 }
@@ -76,4 +59,23 @@ fn get_new_id<T>(vec: &mut Vec<Option<T>>) -> usize {
 
     vec.push(None);
     vec.len() - 1
+}
+
+fn new_manager_creator(podes_rc: Arc<Mutex<Vec<Option<manager::Manager>>>>, listener_rx: mpsc::Receiver<TcpStream>, man_tx: mpsc::Sender<manager::Message>) -> thread::JoinHandle<Result<(), Error>> {
+    thread::spawn(move || -> Result<(), Error> {
+        for stream in listener_rx.iter() {
+            let mut podes = podes_rc.lock().unwrap();
+
+            let id = get_new_id(&mut podes);
+
+            let mut manager = manager::Manager::new(id, stream, man_tx.clone());
+            manager.manage()?;
+
+            podes[id] = Some(manager);
+        }
+
+        println!("Channel from listener closed. Exiting...");
+
+        Ok(())
+    })
 }
