c23522f4a101c0ac4a97f96fd1fdd606927f93fd
GONI Guillaume <gdn3@live.fr>
2018-09-12 21:15:24 +0200

    host functions in wasm interpreter and passing strings

--- balthapode/src/wasm.rs
+++ balthapode/src/wasm.rs
@@ -1,4 +1,67 @@
-use wasmi::{ImportsBuilder, Module, ModuleInstance, NopExternals, RuntimeArgs, RuntimeValue};
+use wasmi::{
+    Error as InterpreterError, Externals, FuncInstance, FuncRef, ImportsBuilder, Module,
+    ModuleImportResolver, ModuleInstance, RuntimeArgs, RuntimeValue, Signature, Trap,
+};
+
+//TODO: clean this mess!!!
+//TODO: unwrap to proper errors
+
+struct Runtime {
+    txt: Vec<u8>,
+}
+
+impl Externals for Runtime {
+    fn invoke_index(
+        &mut self,
+        index: usize,
+        args: RuntimeArgs,
+    ) -> Result<Option<RuntimeValue>, Trap> {
+        match index {
+            0 => {
+                let mut args_vec: Vec<u8> = Vec::with_capacity(32);
+                for i in 0..args.len() {
+                    args_vec.push(args.nth_checked(i).unwrap());
+                }
+
+                print!("hash: 0x");
+                args_vec.iter().for_each(|byte| print!("{:x}", byte));
+                println!("");
+            }
+            1 => {
+                let byte: u8 = args.nth_checked(0).unwrap();
+                self.txt.push(byte);
+            }
+            _ => return Err(Trap::new(wasmi::TrapKind::UnexpectedSignature)),
+        }
+
+        Ok(None)
+    }
+}
+
+struct RuntimeModuleImportResolver;
+
+impl<'a> ModuleImportResolver for RuntimeModuleImportResolver {
+    fn resolve_func(
+        &self,
+        field_name: &str,
+        signature: &Signature,
+    ) -> Result<FuncRef, InterpreterError> {
+        // println!("{} {:?}", field_name, signature);
+        let func_ref = match field_name {
+            // TODO: manually do the signature?
+            // TODO: dynamically fetch the index?
+            "return_32bytes" => FuncInstance::alloc_host(signature.clone(), 0),
+            "push_char" => FuncInstance::alloc_host(signature.clone(), 1),
+            _ => {
+                return Err(InterpreterError::Function(format!(
+                    "host module doesn't export function with name {}",
+                    field_name
+                )))
+            }
+        };
+        Ok(func_ref)
+    }
+}
 
 pub fn get_functions_list(bytecode: Vec<u8>) {
     let module: parity_wasm::elements::Module =
@@ -18,19 +81,27 @@ pub fn get_functions_list(bytecode: Vec<u8>) {
 pub fn exec_wasm(bytecode: Vec<u8>) -> Result<u8, u8> {
     let module = Module::from_buffer(&bytecode).expect("test");
 
-    let instance = ModuleInstance::new(&module, &ImportsBuilder::default())
+    let resolver = RuntimeModuleImportResolver;
+    let imports = ImportsBuilder::new().with_resolver("env", &resolver);
+    let instance = ModuleInstance::new(&module, &imports)
         .expect("failed to instantiate wasm module")
         .assert_no_start();
 
+    let mut runtime = Runtime { txt: Vec::new() };
     let res = instance
-            .invoke_export("start", &[], &mut NopExternals)
-            .expect("failed to execute export");
+        .invoke_export("start", &[], &mut runtime)
+        .expect("failed to execute export");
 
     if let Some(RuntimeValue::I32(res)) = res {
-        println!(
-            "start : {:x}", res
-        );
+        println!("start : {:x}", res);
+    } else {
+        println!("start : {:?}", res);
     }
+
+    println!(
+        "hash src: {}",
+        String::from_utf8_lossy(runtime.txt.as_slice())
+    );
 
     Ok(0)
 }
--- balthwasm/src/lib.rs
+++ balthwasm/src/lib.rs
@@ -1,41 +1,35 @@
-extern crate generic_array;
 extern crate sha3;
 
-use generic_array::{typenum::U32, GenericArray};
 use sha3::{Digest, Sha3_256};
 
-// extern "C" {
-//     fn log(s: &str);
-// }
-
-// #[no_mangle]
-// pub unsafe extern "C" fn greet(name: &str) {
-//     log(format!("Hello {}!", String::from(name)).as_str());
-// }
-//
-// #[no_mangle]
-// pub unsafe extern "C" fn len(name: *mut c_char) -> usize {
-//     CString::from_raw(name).into_bytes().len()
-// }
+extern "C" {
+    fn push_char(s: u8);
+    fn return_32bytes(s: [u8; 32]);
+}
 
 #[no_mangle]
 pub extern "C" fn start() -> u32 {
     let res = add(3, 2);
 
     let mut hasher = Sha3_256::default();
-    hasher.input(format!("{}", res).as_bytes());
+    //hasher.input(format!("{}", res).as_bytes());
+    hasher.input(b"test");
 
-    let out = hasher.result();
+    let hash = hasher.result();
 
-    //out[0] as u32
-    digest_to_u32(out)
-}
+    let mut array_hash: [u8; 32] = [0; 32];
 
-fn digest_to_u32(digest: GenericArray<u8, U32>) -> u32 {
-    let mut res = 0;
+    for i in 0..32 {
+        array_hash[i] = hash[i];
+    }
 
-    for i in 0..4 {
-        res = res * 256 + digest[i] as u32;
+    unsafe {
+        return_32bytes(array_hash);
+        let string = String::from("test");
+        string
+            .into_bytes()
+            .iter()
+            .for_each(|c| push_char(c.clone()));
     }
 
     res
