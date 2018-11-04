8c5207a9dbc6f10c4d1119a7a06df14826b80c19
GONI Guillaume <gdn3@live.fr>
2018-11-05 00:11:53 +0100

    finished message_code.rs for tokio

--- balthernet/src/asynctest/message_codec.rs
+++ balthernet/src/asynctest/message_codec.rs
@@ -1,10 +1,15 @@
-use bytes::BytesMut;
+use bytes::{BufMut, BytesMut};
 use ron::{de, ser};
 use tokio::codec::{Decoder, Encoder};
 use tokio::io;
 
 use balthmessage as message;
-use balthmessage::Message;
+use balthmessage::{Error, Message};
+
+// TODO: As parameters...
+const MESSAGE_SIZE_LIMIT: usize = 2 << 20;
+// TODO: Is there a window between JOB_SIZE_LIMIT converted and MESSAGE_SIZE_LIMIT?
+const JOB_SIZE_LIMIT: usize = MESSAGE_SIZE_LIMIT >> 2;
 
 pub struct MessageCodec {
     msg_size: Option<usize>,
@@ -18,7 +23,7 @@ impl MessageCodec {
 
 impl Decoder for MessageCodec {
     type Item = Message;
-    type Error = message::Error;
+    type Error = Error;
 
     fn decode(&mut self, buf: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
         if let None = self.msg_size {
@@ -66,9 +71,9 @@ impl Decoder for MessageCodec {
 
 impl Encoder for MessageCodec {
     type Item = Message;
-    type Error = message::Error;
+    type Error = Error;
 
-    fn encode(&mut self, message: Message, buf: &mut BytesMut) -> Result<(), message::Error> {
+    fn encode(&mut self, message: Message, buf: &mut BytesMut) -> Result<(), Error> {
         /*
         // It's important to reserve the amount of space needed. The `bytes` API
         // does not grow the buffers implicitly.
@@ -84,6 +89,38 @@ impl Encoder for MessageCodec {
         
         // Return ok to signal that no error occured.
         */
+        // This prevents spending time to convert the task... :
+        // TODO: same with Task ?
+        if let Message::Job(_, bytecode) = &message {
+            if bytecode.len() >= JOB_SIZE_LIMIT as usize {
+                return Err(Error::JobTooBig(bytecode.len()));
+            }
+        }
+
+        let msg_str = ser::to_string(&message)?;
+        let len = msg_str.len();
+
+        match message {
+            Message::Job(job_id, _) => println!("sending Job #{} of {} bytes.", job_id, len),
+            Message::Task(job_id, task_id, _) => println!(
+                "sending Task #{} for Job #{} of {} bytes.",
+                task_id, job_id, len
+            ),
+            Message::ReturnValue(job_id, task_id, _) => println!(
+                "sending result for Task #{} for Job #{} of {} bytes.",
+                task_id, job_id, len
+            ),
+            _ => println!("sending `{}` of {} bytes.", msg_str, len),
+        }
+
+        if len >= MESSAGE_SIZE_LIMIT as usize {
+            return Err(Error::MessageTooBig(len));
+        }
+
+        buf.reserve(len - buf.len() + 4);
+        buf.put_u32_le(len as u32);
+        buf.put_slice(msg_str.as_bytes());
+
         Ok(())
     }
 }
