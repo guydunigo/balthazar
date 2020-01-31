3685c04849b3bad6a6d66cc832b21f34fb943138
GONI Guillaume <gdn3@live.fr>
2020-01-31 14:48:54 +0100

    started ipfs integration

---
+++ balthastore/src/ipfs.rs
@@ -0,0 +1,9 @@
+extern crate ipfs_api;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
--- balthastore/src/lib.rs
+++ balthastore/src/lib.rs
@@ -1,3 +1,14 @@
+pub mod ipfs;
+
+pub type FileAddr = String;
+
+pub trait Storage {
+    type Error: Error;
+
+    fn store(data: &[u8]) -> Result<FileAddr, Self::Error>;
+    fn get(addr: &FileAddr) -> Result<Vec<u8>, Self::Error>;
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
