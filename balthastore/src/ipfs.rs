d9b5c98e61e68c6d08508b035df57a985e901580
GONI Guillaume <gdn3@live.fr>
2020-01-31 22:45:44 +0100

    implemented ipfs?

--- balthastore/src/ipfs.rs
+++ balthastore/src/ipfs.rs
@@ -1,4 +1,79 @@
+extern crate http;
 extern crate ipfs_api;
+
+use bytes::Bytes;
+use futures::{
+    future::BoxFuture, io::AsyncRead, stream::BoxStream, FutureExt, StreamExt, TryStreamExt,
+};
+use http::uri::InvalidUri;
+use ipfs_api::response;
+use ipfs_api::IpfsClient;
+use multiaddr::Multiaddr;
+use std::{error::Error, fmt, io};
+
+use super::{FileAddr, Storage};
+
+/// Wrapper arround [`ipfs_api::response::Error`] to implement trait [`std::error:Error`].
+#[derive(Debug)]
+pub struct IpfsApiResponseError {
+    inner: response::Error,
+}
+impl fmt::Display for IpfsApiResponseError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.inner)
+    }
+}
+impl Error for IpfsApiResponseError {}
+impl From<response::Error> for IpfsApiResponseError {
+    fn from(src: response::Error) -> Self {
+        IpfsApiResponseError { inner: src }
+    }
+}
+
+/// Ipfs storage for use by the nodes.
+/// Creating it through the [`Default::default`] trait connects to the default Ipfs
+/// port on `localhost:5001`.
+#[derive(Clone, Default)]
+pub struct IpfsStorage {
+    ipfs_client: IpfsClient,
+}
+
+impl IpfsStorage {
+    /// Creates a new client connecting to the listening multiaddr.
+    pub fn new(listen_addr: Multiaddr) -> Result<Self, InvalidUri> {
+        Ok(IpfsStorage {
+            ipfs_client: IpfsClient::new_from_uri(&listen_addr.to_string()[..])?,
+        })
+    }
+
+    /// Return the inner [`IpfsClient`] if a direct access is needed.
+    pub fn into_inner(&self) -> &IpfsClient {
+        &self.ipfs_client
+    }
+}
+
+impl Storage for IpfsStorage {
+    type Error = IpfsApiResponseError;
+
+    fn store_stream<T: 'static + io::Read + AsyncRead + Send + Sync>(
+        &self,
+        data_stream: T,
+    ) -> BoxFuture<Result<FileAddr, Self::Error>> {
+        async move {
+            let res = self.ipfs_client.add(data_stream).await;
+            println!("Stored on IPFS : {:?}", res);
+            match res {
+                Ok(res) => Ok(res.name),
+                Err(error) => Err(error.into()),
+            }
+        }
+        .boxed()
+    }
+
+    fn get_stream(&self, addr: &FileAddr) -> BoxStream<Result<Bytes, Self::Error>> {
+        self.ipfs_client.get(addr).map_err(|e| e.into()).boxed()
+    }
+}
 
 #[cfg(test)]
 mod tests {
--- balthastore/src/lib.rs
+++ balthastore/src/lib.rs
@@ -1,12 +1,47 @@
+extern crate bytes;
+extern crate futures;
+extern crate multiaddr;
+
+use bytes::Bytes;
+use futures::{future::BoxFuture, io::AsyncRead, stream::BoxStream, FutureExt, StreamExt};
+use std::{error::Error, io};
+
 pub mod ipfs;
 
 pub type FileAddr = String;
 
-pub trait Storage {
+/// This trait defines a generic interface for storage mechanisms so they can be used interchangeably.
+pub trait Storage: Sync {
     type Error: Error;
 
-    fn store(data: &[u8]) -> Result<FileAddr, Self::Error>;
-    fn get(addr: &FileAddr) -> Result<Vec<u8>, Self::Error>;
+    /// Stores provided data from the Storage coming from an async stream.
+    fn store_stream<T: 'static + io::Read + AsyncRead + Send + Sync>(
+        &self,
+        data_stream: T,
+    ) -> BoxFuture<Result<FileAddr, Self::Error>>;
+    /// Get the requested data from the Storage as a [`futures::Stream`]
+    fn get_stream(&self, addr: &FileAddr) -> BoxStream<Result<Bytes, Self::Error>>;
+
+    /// Same as [`Storage::store_stream`] but to provide all the data as once.
+    fn store(&self, data: &'static [u8]) -> BoxFuture<Result<FileAddr, Self::Error>> {
+        self.store_stream(data)
+    }
+
+    /// Same as [`Storage::get_stream`] but to get all the data as once.
+    fn get<'a>(&'a self, addr: &'a FileAddr) -> BoxFuture<'a, Result<Bytes, Self::Error>> {
+        // TODO: not very efficient ?
+        let mut tmp = Vec::new();
+        async move {
+            while let Some(chunk_res) = self.get_stream(addr).next().await {
+                match chunk_res {
+                    Ok(chunk) => tmp.extend_from_slice(&chunk[..]),
+                    Err(error) => return Err(error),
+                }
+            }
+            Ok(Bytes::from(tmp))
+        }
+        .boxed()
+    }
 }
 
 #[cfg(test)]
