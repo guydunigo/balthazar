9d852566edee94b52ab57a52cf70d491597533b5
GONI Guillaume <gdn3@live.fr>
2020-02-27 11:17:57 +0100

    hash generator algorithm for wasm program

--- balthwasm/src/lib.rs
+++ balthwasm/src/lib.rs
@@ -1,3 +1,7 @@
+extern crate multihash;
+
+use multihash::Keccak256;
+
 mod abi;
 use abi::LocalResult;
 
@@ -5,7 +9,29 @@ use abi::LocalResult;
 //                          User's code
 // ------------------------------------------------------------------
 
-fn my_run(arguments: Vec<u8>) -> LocalResult<Vec<u8>> {
+pub fn my_run(arguments: Vec<u8>) -> LocalResult<Vec<u8>> {
+    // TODO: checking arguments.
+
+    for counter in 0.. {
+        let string = format!("{}", counter);
+        let hash_bytes = Keccak256::digest(string.as_bytes()).into_bytes();
+        let hash: String = hash_bytes.iter().map(|b| format!("{:02x}", b)).collect();
+
+        if hash
+            .as_bytes()
+            .windows(arguments.len())
+            .any(|w| *w == arguments[..])
+        {
+            let return_string = format!("({}, {})", counter, hash);
+            return Ok(return_string.into_bytes());
+        }
+    }
+
+    Err(-1)
+}
+
+/*
+fn my_run_old(arguments: Vec<u8>) -> LocalResult<Vec<u8>> {
     // decoding arguments
     let args = String::from_utf8_lossy(&arguments[..]);
     let number = args.parse().map_err(|_| 1)?;
@@ -17,3 +43,4 @@ fn my_run(arguments: Vec<u8>) -> LocalResult<Vec<u8>> {
     let res_str = format!("{}", result);
     Ok(res_str.into_bytes())
 }
+*/
